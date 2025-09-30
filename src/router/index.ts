b9ae23e464eb6bdc943f8d46e61b8b4e31cd0938
MiltonDiazCO <diazmartinezmilton@gmail.com>
2025-09-29 21:20:40 -0500

    Configuración inicial y formulario de ingreso

---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      redirect: { name: 'login' },
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/modules/auth/pages/LoginView.vue'),
+    },
+  ],
+});
+
+export default router;
