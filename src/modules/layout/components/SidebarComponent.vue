0f69918150f7dbdf2d7f85bf32126a303a8e17f2
MiltonDiazCO <diazmartinezmilton@gmail.com>
2025-11-08 15:44:32 -0500

    Corrección Bug: Cerrar sidebar al hacer clic sobre uno de los botones

--- src/modules/layout/components/SidebarComponent.vue
+++ src/modules/layout/components/SidebarComponent.vue
@@ -2,11 +2,11 @@
   <div class="offcanvas offcanvas-start" id="sidebar-lateral">
     <div class="offcanvas-body px-0">
       <ul class="navbar-nav fs-5">
-        <li class="navbar-item p-3 py-md-1">
+        <li class="navbar-item p-3 py-md-1" data-bs-dismiss="offcanvas">
           <router-link :to="{ name: 'mi-perfil' }" class="nav-link">Mi Perfil</router-link>
         </li>
 
-        <li class="navbar-item p-3 py-md-1">
+        <li class="navbar-item p-3 py-md-1" data-bs-dismiss="offcanvas">
           <router-link to="/inversiones" class="nav-link">Inversiones</router-link>
         </li>
 
@@ -24,17 +24,17 @@
 
           <div class="collapse" id="submenu-atajos">
             <ul class="navbar-nav fs-6 px-3">
-              <li class="navbar-item">
-                <router-link to="" class="nav-link">Nuevo Movimiento</router-link>
+              <li class="navbar-item" data-bs-dismiss="offcanvas">
+                <router-link to="" class="nav-link">Nuevo Movimiento </router-link>
               </li>
-              <li class="navbar-item">
-                <router-link to="" class="nav-link">Nueva Actividad</router-link>
+              <li class="navbar-item" data-bs-dismiss="offcanvas">
+                <router-link to="" class="nav-link">Nueva Actividad </router-link>
               </li>
             </ul>
           </div>
         </li>
 
-        <li class="navbar-item p-3 py-md-1">
+        <li class="navbar-item p-3 py-md-1" data-bs-dismiss="offcanvas">
           <router-link to="" class="nav-link">Actividades Recientes</router-link>
         </li>
       </ul>
